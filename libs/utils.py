25aa1c5493dc72c9132d2a1b7f3c9581c63ad6f8
Boop189 <brenden111@hotmail.com>
2019-09-09 15:22:39 -0400

    changed file name to utils -> will contain utils

--- libs/utils.py
+++ libs/utils.py
@@ -5,7 +5,7 @@ import json
 import logging
 import requests
 from var.telemetry import DigitalOceanRequests
-from var.vars import DO_REGIONS, DO_BASE_URL
+from var.vars import DO_REGIONS, DO_BASE_URL, DO_SIZES
 
 #Droplet creation and deletetion
 
@@ -20,9 +20,17 @@ alt_headers = {
 }
 request_object = DigitalOceanRequests(DO_BASE_URL, os.environ['DO_AUTH'])
 
-class Regions():
+class Utilities():
     def list_all_do_regions(self):
-        r = request_object.digital_ocean_get_endpoint(endpoint_url=DO_REGIONS)
-        return r
-regions = Regions()
+        try:
+            r = request_object.digital_ocean_get_endpoint(endpoint_url=DO_REGIONS)
+            return r
+        except requests.ConnectionError as e:
+            logging.info("ERROR: Connection failed")
+    def list_all_do_sizes(self):
+        try:
+            r = request_object.digital_ocean_get_endpoint(endpoint_url=DO_SIZES)
+        except requests.ConnectionError as e:
+            logging.info("ERROR: Connection failed.")
+regions = Utilities()
 print(regions.list_all_do_regions())
