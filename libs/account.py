dd543b77294eb244a10a6fef6e2d247e5a31eddd
Unknown <azettler@live.com>
2019-09-15 09:38:59 -0400

    Droplet creation from new sample data
    Dataclass implemented for Droplet, Region, and Image.
    from_json methods for each class mentioned.
    Work with telemetry call standardization.

--- libs/account.py
+++ libs/account.py
@@ -3,9 +3,10 @@ import sys
 import requests
 import json
 import logging
-from var.telemetry import DigitalOceanRequests
+from var.telemetry import DigitalOceanRequests, do_requests
+from var.vars import *
 
-#For account specific calls.
+# For account specific calls.
 
 logging.basicConfig(
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -13,28 +14,26 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-DO_BASE_URL = "https://api.digitalocean.com"
-DO_ACCOUNT = "/v2/account"
-req_object = DigitalOceanRequests(DO_BASE_URL, os.environ['DO_AUTH'])
+# DO_BASE_URL = "https://api.digitalocean.com"
+# DO_ACCOUNT = "/v2/account"
+req_object = do_requests  # DigitalOceanRequests(DO_BASE_URL, os.environ['DO_AUTH'])
 
-class Account():
+
+class Account(object):
     def check_account_status(self, url_endpoint):
         response_data = {}
         r = req_object.digital_ocean_get_endpoint(DO_ACCOUNT)
-        response_data['status'] = json.dumps(r['account']['status']).replace('"','')
+        response_data['status'] = json.dumps(r['account']['status']).replace('"', '')
         return response_data['status']
 
     def get_account_info(self, url_endpoint):
         json_response = {}
         account_info = req_object.digital_ocean_get_endpoint(DO_ACCOUNT)
         for key in account_info:
-            json_response[key] = json.dumps(account_info[key]).replace('"','')
+            json_response[key] = json.dumps(account_info[key]).replace('"', '')
         return json_response
-a_info = Account()
-print(a_info.check_account_status(DO_ACCOUNT))
-
-        
 
 
-
-
+if __name__ == '__main__':
+    a_info = Account()
+    print(a_info.check_account_status(DO_ACCOUNT))
